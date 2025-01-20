ae9c90f0bdb0961a0fad14e25b69a1afc34323c5
sourav-unibit <souravdas@unibit.in>
2025-01-20 13:44:15 +0000

    processing

--- controllers/bits.controller.js
+++ controllers/bits.controller.js
@@ -9,12 +9,11 @@ exports.matchBitsHandler = async () => {
             return;
         }
         const matchBits = getMatchBits(unMatchBitDbRes.data);
-        console.log(matchBits)
         if (!matchBits) {
             return;
         }
-        for (let key in matchBits) {
-            console.log(key,matchBits[key].yes.sort((a,b)=>a.amount-b.amount) != matchBits[key].no.sort((a,b)=>a.amount-b.amount))
+        for (let key in matchBits) { //key means eventId
+            // console.log(key,matchBits[key].yes.sort((a,b)=>a.amount-b.amount) , matchBits[key].no.sort((a,b)=>b.amount-a.amount))
             if (matchBits[key].yes.length != matchBits[key].no.length) {
                 continue;
             }
@@ -31,17 +30,15 @@ function getMatchBits(bits) {
     let matchEventInfo = {};
     let yesMap = new Map();
     let noMap = new Map();
-    let isAnyMatch = false;
+    let isAnyMatch = { match: false };
     for (let bitItem of bits) {
         if (bitItem.choose_option_id == dbConstant.mysql.bits.choose_option_id.yes) {
-            mapOperationOfGetMatchBits(bitItem, noMap, yesMap, matchEventInfo);
+            mapOperationOfGetMatchBits(bitItem, noMap, yesMap, matchEventInfo, isAnyMatch);
         }
         else if (bitItem.choose_option_id == dbConstant.mysql.bits.choose_option_id.no) {
-            mapOperationOfGetMatchBits(bitItem, yesMap, noMap, matchEventInfo)
+            mapOperationOfGetMatchBits(bitItem, yesMap, noMap, matchEventInfo, isAnyMatch)
         }
     }
-    console.log(matchEventInfo)
-    //todo check how isAnyMatch variable asign in fun
     if (!isAnyMatch) {
         return null
     }
@@ -49,10 +46,11 @@ function getMatchBits(bits) {
 
 }
 
-function mapOperationOfGetMatchBits(bitItem, getMap, setMap, matchEventInfo) {
+function mapOperationOfGetMatchBits(bitItem, getMap, setMap, matchEventInfo, isAnyMatch) {
     let getMapKey = JSON.stringify([bitItem.event_id, 10 - bitItem.amount])
     let isExitBitAmount = getMap.get(getMapKey)
     if (isExitBitAmount) {
+        isAnyMatch.match = true;
         if (!matchEventInfo[bitItem.event_id]) {
             matchEventInfo[bitItem.event_id] = { yes: [], no: [] }
         }
@@ -63,7 +61,7 @@ function mapOperationOfGetMatchBits(bitItem, getMap, setMap, matchEventInfo) {
     } else {
         setMap.set(JSON.stringify([bitItem.event_id, bitItem.amount]), { totalCount: bitItem.total_count, amount: bitItem.amount });
     }
-    
+
 }
 
 async function updateMatchBit(eventId, yesArr, noArr) {
@@ -75,7 +73,57 @@ async function updateMatchBit(eventId, yesArr, noArr) {
         bitsPromise.push(bitsDb.allMatch(eventId, dbConstant.mysql.bits.choose_option_id.no, noItem.amount, noItem.limit))
     }
     const bitsPromiseRes = await Promise.all(bitsPromise);
-    console.log(bitsPromiseRes[0])
+
+    let yesMap = new Map();
+    let noMap = new Map();
+    let newTreads = [];
+    let matchBitIds = [];
+    for (let bitItemArr of bitsPromiseRes) {
+        for (let bitItem of bitItemArr.data) {
+            if (bitItem.choose_option_id == 1) {
+                mapOperationOfUpdateMatchBit(bitItem, noMap, yesMap, matchBitIds, newTreads)
+            }
+            else if (bitItem.choose_option_id == 2) {
+                mapOperationOfUpdateMatchBit(bitItem, yesMap, noMap, matchBitIds, newTreads)
+            }
+        }
+    }
+    if(matchBitIds.length===0||newTreads.length==0){
+        return;
+    }
+    // bitsDb.updateMatchIdsStatus(matchBitIds)
+    
+    //Todo create treads model
+}
+
+function mapOperationOfUpdateMatchBit(bitItem, getMap, setMap, matchBitIds, newTreads) {
+    let getMapKey = 10 - bitItem.amount;
+    let isBitExist = getMap.get(getMapKey);
+    let existBitIndex = isBitExist ? isBitExist.findIndex((item) => item.user_id != bitItem.user_id) : -1
+    if (existBitIndex != -1) {
+        const existBitData = isBitExist.splice(existBitIndex, 1)[0];
+        matchBitIds.push(existBitData.id);
+        matchBitIds.push(bitItem.id);
+        storeNewTreads(newTreads, bitItem.event_id, bitItem, existBitData);
+        getMap.set(getMapKey, isBitExist)
+    } else {
+        const setMapKey = bitItem.amount;
+        let setData = [];
+        const isSetMapExist = setMap.get(setMapKey);
+        if (isSetMapExist) {
+            setData = isSetMapExist
+        }
+        setData.push(bitItem)
+        setMap.set(bitItem.amount, setData)
+    }
+}
+
+function storeNewTreads(newTreads, eventId, yesObj, noObj) {
+    const newTread = {
+        eventId: eventId,
+        bits: [yesObj, noObj]
+    }
+    newTreads.push(newTread)
 }
 
 this.matchBitsHandler()
--- db/mysql/bits.db.js
+++ db/mysql/bits.db.js
@@ -30,11 +30,12 @@ exports.allUnMatch = () => {
     })
 }
 
-exports.allMatch = (eventId, optionId, amount,limit) => {
+exports.allMatch = (eventId, optionId, amount, limit) => {
+    // console.log(eventId, optionId, amount,limit)
     return new Promise((resolve) => {
         const status = dbConstant.mysql.bits.status.unMatch;
         const sqlQuery = `SELECT id,user_id,user_name,user_pic,event_id,amount,choose_option_id,created_date FROM bits WHERE status=? AND event_id = ? AND choose_option_id=? AND amount = ? ORDER BY id asc LIMIT ? `
-        connection.query(sqlQuery, [status, eventId, optionId, amount, limit], (error, result) => { 
+        connection.query(sqlQuery, [status, eventId, optionId, amount, limit], (error, result) => {
             if (error) {
                 log(error)
                 return resolve({
@@ -47,6 +48,36 @@ exports.allMatch = (eventId, optionId, amount,limit) => {
                 return resolve({
                     error: true,
                     message: 'not bits found',
+                    data: null
+                })
+            }
+            return resolve({
+                error: false,
+                message: "bits fetch successfully",
+                data: result
+            })
+        })
+    })
+}
+
+exports.updateMatchIdsStatus = (bitIds) => {
+    return new Promise((resolve) => {
+        const matchStatus = dbConstant.mysql.bits.status.match;
+        const unMatchStatus = dbConstant.mysql.bits.status.unMatch;
+        const sqlQuery = `UPDATE bits SET status=? WHERE status = ? AND id IN ? `
+        connection.query(sqlQuery, [bitIds, matchStatus, unMatchStatus], (error, result) => {
+            if (error) {
+                log(error)
+                return resolve({
+                    error: true,
+                    message: error.message,
+                    data: null
+                })
+            }
+            if (result.affectedRows!=bitIds.length) {
+                return resolve({
+                    error: true,
+                    message: `${bitIds.length} number of bitIds coming but update ${result.affectedRows} `,
                     data: null
                 })
             }
